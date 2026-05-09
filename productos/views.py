9468abd0367429798b823995368e1e7840ecc816
Cristian López <60026545+cristianL0pez@users.noreply.github.com>
2026-05-09 01:20:47 +0000

    beer

--- productos/views.py
+++ productos/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Producto
+from .models import Producto, cerveza
 
 # Create your views here.
 def index(request):
     productos = Producto.objects.all()#trae los productos desde la db como obj 
     return render(request, 'productos/home.html',{'productos':productos})
     
+def lista_cervezas(request):
+    cervezas = cerveza.objects.all()
+    return render(request,'beer/beer.html',{'cervezas':cervezas})
 
 
 def dashboard(request):
